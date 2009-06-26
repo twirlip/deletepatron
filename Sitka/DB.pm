8ea51792cd949f7cf2a8baad87ca129b1aa5944f
Jeff Davis <jeff@textsfornothing.com>
2009-06-26 13:32:57 -0700

    lookup database connect info in config files rudiments of an auth mechanism

--- Sitka/DB.pm
+++ Sitka/DB.pm
@@ -1,15 +1,25 @@
 #!/usr/bin/perl
 package Sitka::DB;
 use DBI;
+use OpenSRF::System;
+use OpenSRF::Utils::SettingsClient;
 
-# TODO: integrate this with fm_IDL.xml
-# database connection
-my ($db_driver,$db_host,$db_name,$db_user,$db_pw) =
-  #('Pg','192.168.0.210','evergreen','evergreen','f00bcp1n35b4r');
-  ('Pg','192.168.0.219','husk','postgres','bcp1n35f00b4r');
-my $dsn = "dbi:$db_driver:host=$db_host;dbname=$db_name";
-$dbh = DBI->connect($dsn,$db_user,$db_pw);
-$dbh->do("BEGIN;") or die "Could not begin database transaction $dsn"; 
+sub new {
+# get database connection info from fm_IDL.xml
+  OpenSRF::System->bootstrap_client( config_file => '/openils/conf/opensrf_core.xml');
+  my $settings  = OpenSRF::Utils::SettingsClient->new;
+  my $db_driver = $settings->config_value(apps => 'open-ils.storage' => 'app_settings' => 'databases' => 'driver');
+  my $db_host   = $settings->config_value(apps => 'open-ils.storage' => 'app_settings' => 'databases' => 'database' => 'host');
+  my $db_name   = $settings->config_value(apps => 'open-ils.storage' => 'app_settings' => 'databases' => 'database' => 'db');
+  my $db_user   = $settings->config_value(apps => 'open-ils.storage' => 'app_settings' => 'databases' => 'database' => 'user');
+  my $db_pw     = $settings->config_value(apps => 'open-ils.storage' => 'app_settings' => 'databases' => 'database' => 'pw');
+
+  # database connection
+  my $dsn = "dbi:$db_driver:host=$db_host;dbname=$db_name";
+  $dbh = DBI->connect($dsn,$db_user,$db_pw);
+  $dbh->do("BEGIN;") or die "Could not begin database transaction $dsn"; 
+  return $dbh;
+}
 
 sub lookup {
   my $sql = shift @_;
--- auth.cgi
+++ auth.cgi
@@ -1,1 +1,17 @@
 #!/usr/bin/perl
+# a horribly hacked-together authentication mechanism for the Sitka deletepatron system
+use CGI;
+use Sitka::DB;
+
+fail('Please login.') unless (param()); # TODO: write a messaging system to handle fail() msgs
+
+my $usr = param('usr');
+my $pwd = param('pwd');
+
+my $db = Sitka::DB->new;
+my @usr = $db->lookup('SELECT usr, profile, home_ou FROM actor.usr WHERE usrname = ? AND passwd = md5(?);', $usr, $pwd);
+
+fail('Username or password incorrect.') unless (@usr);
+
+# TODO: Flag this session as authenticated, set $ou, and proceed to lookup.cgi
+
