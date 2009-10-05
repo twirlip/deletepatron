e5f041a0e967ce3af9eeab166731ad5aa2ca3f81
twirlip <twirlip@twirlip.org>
2009-10-05 13:09:31 -0700

    break up Sitka::Session into smaller methods

--- Sitka/Session.pm
+++ Sitka/Session.pm
@@ -9,30 +9,50 @@ use Sitka::DB;
 use OpenSRF::System;
 use OpenILS::Application::AppUtils;
 
+our @fail;
+
 sub new {
   my $usr = shift;
   my $pwd = shift;
-  my $session = authenticate($usr, $pwd);
-  login() unless ($session);
-  return $session;
+  if (authenticate($usr, $pwd)) {
+    # TODO: user is authenticated; create CGI session
+  } else {
+    login(\@fail);
+  }
 }
 
 sub authenticate {
   my $usr = shift;
   my $pwd = shift;
-  my $usr_id;
-  my $home_ou;
+  my $has_perms = undef; # unset unless user has DELETE_USER permissions
+  my $usrdata = check_password($usr, $pwd);
+  if ($usrdata) {
+    $has_perms = check_perms($usrdata->{usr_id}, $usrdata->{home_ou});
+  }
+  if ($has_perms) {
+    # user is authenticated!
+    # TODO: return a list of OUIDs for which user has permission to delete users
+    return $usrdata;
+  } 
+  return;
+}
+
+sub check_password {
+  my ($usr, $pwd) = @_;
 
   # TODO: do this via OpenSRF API rather than direct DB lookup
   my $db = Sitka::DB->connect();
   my $usrdata = $db->selectrow_hashref("SELECT id, usrname, passwd, home_ou FROM actor.usr WHERE usrname = ? and passwd = md5(?);", undef, ($usr, $pwd));
   if ($usrdata) {
-    $usr_id  = $usrdata->{id};
-    $home_ou = $usrdata->{home_ou};
+    return $usrdata;
   } else {
     push @fail, 'INVALID_LOGIN';
     return;
   }
+}
+
+sub check_perms {
+  my ($usr_id, $home_ou) = @_;
 
   # make sure this user has permission to delete users
   OpenSRF::System->bootstrap_client( config_file => '/openils/conf/opensrf_core.xml');
@@ -41,11 +61,11 @@ sub authenticate {
   if ($p) {
     push @fail, 'MISSING_PERMS: ' . $p->{textcode} . ": " . $p->{desc};
     return;
+  } else {
+    return 1;
   }
+}
 
-  # user is authenticated!
-  # TODO: return a list of OUIDs for which user has permission to delete users
-  return $home_ou;
 }
 
 # TODO: write a proper messaging system in Sitka.pm to handle fail() msgs
@@ -62,9 +82,7 @@ sub fail {
 
 sub login {
   my $msgs = shift;
-  my $destination = shift || undef;
   my $cgi = CGI->new;
-  $destination = $cgi->script_name() if (!$destination);
   print $cgi->header,
         $cgi->start_html('Sitka Patron Deletions - Login'),
         $cgi->h1('Please Login');
@@ -73,7 +91,7 @@ sub login {
       print $cgi->div({-class=>$msgtype},$msgtext);
     }
   }
-  print $cgi->start_form( -method=>'POST', -action=>$destination );
+  print $cgi->start_form( -method=>'POST', -action=>"input.cgi" );
   print $cgi->textfield('usr'),
         $cgi->password_field('pwd'),
         $cgi->submit('submit','Login');
